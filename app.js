ee6442d9a54bcb5c410b34237a8f6c344c08f973
JaviAPereira <pereira.javier97@gmail.com>
2024-05-13 10:45:39 -0300

    prueba de datos json, para comprobar si desde github pages se visualiza correctamente

---
+++ app.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+    loadData();
+}
+
+function loadData(){
+    fetch('data/practica_json.json')
+        .then(Response => Response.json())
+        .then(data => showData(data))
+        .catch(error => console.error('error al cargar losd datos:', error));
+}
+
+function showData(data){
+    const userData = document.getElementById('user-data');
+    userData.innerHTML= ''
+    data.forEach(usuario => {
+        userData.innerHTML += `
+          <div>
+            <p>ID: ${usuario.id}</p>
+            <p>Nombre: ${usuario.nombre}</p>
+            <p>Apellido: ${usuario.apellido}</p>
+            <p>Ciudad: ${usuario.ciudad}</p>
+            <p>Número de Teléfono: ${usuario.numero_de_telefono}</p>
+            <img src= ${usuario.foto} width=40px>
+          </div>
+        `;
+      });
+}
